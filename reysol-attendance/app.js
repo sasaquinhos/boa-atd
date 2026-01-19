a152aa23521ba29d39e2fcea9aff6e33aef84978
sasaquinhos <sasaki@souken-sys.co.jp>
2026-01-19 11:17:16 +0900

    Update app.js
    Signed-off-by: sasaquinhos <sasaki@souken-sys.co.jp>

--- reysol-attendance/app.js
+++ reysol-attendance/app.js
@@ -8,7 +8,7 @@ const state = {
     matchLimit: 10
 };
 
-const API_URL = 'https://script.google.com/macros/s/AKfycbz_Fr2aYZPhgv6WKIxWQDht0g6jR4RL2p4N7ceAcScwyENPM6RyYijrg_YCeBEQqcFVig/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbwjL6Zii0EdQHzlOvQNS1LprDJ8VfGWQ_yrS8jqj63wMT4erN_RXUNKbKTaBzyoBFLjkA/exec';
 
 // DOM Elements
 const matchesContainer = document.getElementById('matches-container');
